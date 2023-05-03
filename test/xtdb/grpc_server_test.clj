5c4ce7eb6d1afe6f1e30c1d4776a3255ed442865
Valery Lavrentiev <valery.lavrentiev@ada.com>
2023-05-03 17:41:52 +0200

    added basic test

---
+++ test/xtdb/grpc_server_test.clj
@@ -0,0 +1,28 @@
+(ns xtdb.grpc-server-test
+  (:require [clojure.test :refer :all]
+            [xtdb.grpc-server :refer :all])
+  (:import (io.grpc ManagedChannelBuilder)
+           [xtdb.grpc.server ServerGrpc]
+           [xtdb.grpc.server StatusRequest]
+           [xtdb.grpc.server StatusReply]))
+
+(def client (ServerGrpc/newBlockingStub
+              (-> (ManagedChannelBuilder/forAddress "localhost" (int default_port))
+                  (.usePlaintext)
+                  .build)))
+
+(defn say-hello []
+  (println
+    (.getMessage
+      (.status client (-> (StatusRequest/newBuilder)
+                          .build)))))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (do
+      (say-hello))
+    ))
+
+
+
+
